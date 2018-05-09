601094932fe99e3ea35970cb6031f79056b1a34a
nbandong <nikolaibandong@gmail.com>
2018-05-08 19:18:36 -0700

    Add processOrderData

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/project/orderConfirm', require('./routes/project/orderConfirm') );
+app.use('/project/processOrderData', require('./routes/project/processOrderData') );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/project/processOrderData.js
+++ routes/project/processOrderData.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/*
 router.post('/', function( request, response ) 
-*/
-router.use( function( request, response, next )
 {
-	var jsonString = JSON.stringify( { x: 5, y: 6 } );
-	response.send( jsonString );
+	//console.log( request.body );
+	
+	response.status(200).send( request.body );
 });
 
 module.exports = router;
