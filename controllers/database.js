75a5f4b5b1f2b80740974a80d529f5945e23798c
nbandong <nikolaibandong@gmail.com>
2018-05-22 00:06:01 -0700

    Test commit.

--- controllers/database.js
+++ controllers/database.js
@@ -2,35 +2,39 @@ var mongodb = require('mongodb');
 
 const MongoClient = require('mongodb').MongoClient;
 
-module.exports.getAllOrders = async function( request, response )
+let DB_USER = "csweb01";
+let DB_PASSWORD = "csweb01";
+let DB_NAME = "csweb01";
+let DB_DOMAIN = "ds014388.mlab.com:14388";
+
+async function connect()
 {
-	let dbuser = "csweb01";
-	let dbpassword = "csweb01";
-	let dbname = "csweb01";
-	let url = "mongodb://" + dbuser + ":" + dbpassword + "@ds014388.mlab.com:14388/" + dbname;
-	
-	//console.log( "Attempting to connect to server" );
+	let url = "mongodb://" + DB_USER + ":" + DB_PASSWORD + "@" + DB_DOMAIN + "/" + DB_NAME;
 	
 	let client = await MongoClient.connect( url );
-	let ret;
 	
-	let connected = client.isConnected( dbname );
+	return client;
+}
+
+module.exports.getAllOrders = async function( request, response )
+{
+	//console.log( "Attempting to connect to server" );
+	
+	let client = await connect();
+	let ret = null;
+	
+	let connected = client.isConnected( DB_NAME );
 	if ( connected )
 	{
 		//console.log( "Connection is good!" );
 		
 		try
 		{
-			const db = client.db( dbname );
-			let collections = await db.collections();
-			
-			//console.log( collections );
+			const db = client.db( DB_NAME );
 			
 			let orders = db.collection( "orders" );
 			
-			//console.log( orders );
-			
-			let allOrders = await orders.find({});
+			let allOrders = await orders.find( {} );
 			ret = allOrders.toArray();
 		}
 		catch ( err )
@@ -45,3 +49,69 @@ module.exports.getAllOrders = async function( request, response )
 	
 	return ret;
 };
+
+// addCustomer() adds a customer to the database. Returns the customer's _id.
+module.exports.addCustomer = async function( info )
+{
+	/*
+		info is an associative array of the structure:
+		
+		{
+			"firstName" : string,
+			"lastName" : string,
+			"address" : string,
+			"address2" : string,
+			"city" : string,
+			"state" : string,
+			"zip" : string,
+			"email" : string
+		}
+	*/
+	
+	let client = await connect();
+	let ret = null;
+	
+	if ( client.isConnected( DB_NAME ) )
+	{
+		try
+		{
+			const db = client.db( DB_NAME );
+			
+			let customers = db.collection( "customers" );
+			
+			console.log( customers );
+			
+			// Check if the customer is already in the collection.
+			
+			let selectQuery = {
+				"_id": true,
+				"email": true
+			}
+			
+			let whereQuery = {
+				"email": info[ "email" ]
+			}
+			
+			let doc = await customers.findOne( whereQuery, selectQuery );
+			
+			console.log( doc );
+			
+			if ( doc == null )
+			{
+				doc = await customers.insertOne( info );
+				
+				console.log( doc );
+			}
+			
+			ret = doc[ "_id" ];
+		}
+		catch ( err )
+		{
+			console.log( err.stack );
+		}
+	}
+	
+	client.close();
+	
+	return ret;
+}
